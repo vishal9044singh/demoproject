7163376060a3c51bb8c9e3c126e3f2898aacb7c4
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-20 19:11:00 +0530

    Create HomeScreen.jsx

---
+++ app/components/HomeScreen.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import HomeNavbar from '../navbars/HomeNavbar';
+import Stories from '../otherComponents/Stories';
+import Posts from '../otherComponents/Posts';
+
+const HomeScreen = () => {
+    return (
+        <ScrollView style={styles.container}>
+            <View style={styles.navbar}>
+                <HomeNavbar />
+            </View>
+
+            <View style={styles.stories}>
+                <Stories />
+            </View>
+
+            <View style={styles.posts}>
+                <Posts />
+                <Posts />
+                <Posts />
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    navbar: {
+        height: '6%'
+    },
+    stories: {
+        height: '10%',
+    }
+});
+
+export default HomeScreen;
