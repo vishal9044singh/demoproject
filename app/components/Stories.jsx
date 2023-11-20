876504c4d13fd20688a02a52d3e374e7a05212f5
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-20 19:12:22 +0530

    Create Stories.jsx

---
+++ app/components/Stories.jsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export default function Stories() {
+    return (
+        <ScrollView style={styles.container} horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.item}>
+                <Text>Item 1</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 2</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 3</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 4</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 5</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 6</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 7</Text>
+            </View>
+            <View style={styles.item}>
+                <Text>Item 8</Text>
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: '100%',
+        width: '100%'
+    },
+    item: {
+        width: 150,
+        height: '90%',
+        margin: 10,
+        backgroundColor: 'lightgrey',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+    },
+});
