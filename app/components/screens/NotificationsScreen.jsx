afd4c9f0eaa75c69b7c6921fcd339abb0158b94a
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-21 18:26:09 +0530

    Create NotificationsScreen.jsx

---
+++ app/components/screens/NotificationsScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function NotificationScreen() {
+    return (
+        <View>
+            <Text>This is NotificationScreen</Text>
+        </View>
+    )
+}
