047e33ae28883ad3d4f02cb4b2471c74d9f6128a
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-21 18:27:39 +0530

    Update HomeScreen.jsx

--- app/components/screens/HomeScreen.jsx
+++ app/components/screens/HomeScreen.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ScrollView, View, StyleSheet } from 'react-native';
-import HomeNavbar from '../navbars/HomeScreenNavbar';
+import HomeScreenNavbar from '../navbars/HomeScreenNavbar';
 import Stories from '../otherComponents/Stories';
+import NotificationScreen from './NotificationScreen';
+import MessageScreen from './MessageScreen';
 import Posts from '../otherComponents/Posts';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const HomeScreen = () => {
+const Stack = createStackNavigator();
+
+function MainScreen() {
     return (
         <SafeAreaView style={styles.container}>
-                <HomeNavbar />
+            <HomeScreenNavbar />
             <ScrollView >
-                    <Stories />
+                <Stories />
                 <View style={styles.posts}>
                     <Posts />
                     <Posts />
@@ -18,6 +23,16 @@ const HomeScreen = () => {
                 </View>
             </ScrollView>
         </SafeAreaView>
+    )
+}
+
+export default function HomeScreen() {
+    return (
+        <Stack.Navigator >
+            <Stack.Screen name="MainScreen" component={MainScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Notifications" component={NotificationScreen} options={{ title: 'Notifications' }} />
+            <Stack.Screen name="Messages" component={MessageScreen} options={{ title: 'Messages' }} />
+        </Stack.Navigator>
     );
 };
 
@@ -26,5 +41,3 @@ const styles = StyleSheet.create({
         flex: 1,
     }
 });
-
-export default HomeScreen;
