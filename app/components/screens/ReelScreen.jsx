78dabaf746b1348616f68e8103dac2470f4eaaff
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-21 18:25:06 +0530

    Update ReelScreen.jsx

--- app/components/screens/ReelScreen.jsx
+++ app/components/screens/ReelScreen.jsx
@@ -1,18 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ReelScreenNavbar from '../navbars/ReelScreenNavbar';
 
 export default function ReelScreen() {
     return (
-        <View style={styles.container}>
-            <Text>This is Reel Screen!</Text>
-        </View>
-    )
-}
+        <SafeAreaView style={styles.container}>
+            <ReelScreenNavbar/>
+            <ScrollView >
+                <Text style={styles.about}>This is Reel Screen!</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    },
+    about: {
+        textAlign: 'center',
+        marginTop: 200
     }
 });
+
