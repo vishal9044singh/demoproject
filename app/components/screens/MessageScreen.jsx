6546636275533154aa70c2b7e46b98165b754c6a
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-21 18:27:03 +0530

    Create MessageScreen.jsx

---
+++ app/components/screens/MessageScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export default function MessageScreen() {
+    return (
+        <View>
+            <Text>This is Message Screen</Text>
+        </View>
+    )
+}
