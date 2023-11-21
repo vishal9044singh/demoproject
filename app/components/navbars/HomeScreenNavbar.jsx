568bd7ba9ecd1cc3f5c2d27d2e903869e29952a3
vishal9044singh <123008369+vishal9044singh@users.noreply.github.com>
2023-11-21 18:32:32 +0530

    Update HomeScreenNavbar.jsx

--- app/components/navbars/HomeScreenNavbar.jsx
+++ app/components/navbars/HomeScreenNavbar.jsx
@@ -1,13 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
 
 export default function HomeScreenNavbar() {
+  const navigation = useNavigation();
   return (
     <View style={styles.navbar}>
       <Text style={styles.heading}>Instagram</Text>
       <View style={styles.rightbar}>
-        <AntDesign style={{ marginRight: 10 }} name="hearto" size={26} color="black" />
-        <AntDesign name="message1" size={26} color="black" />
+        <AntDesign style={{ marginRight: 10 }} name="hearto" size={24} color="black" onPress={() => navigation.navigate('Notifications')} />
+        <AntDesign name="message1" size={24} color="black" onPress={() => navigation.navigate('Messages')} />
       </View>
     </View>
   )
@@ -26,16 +29,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.3,
     shadowRadius: 2,
-},
+  },
   heading: {
     fontSize: 25,
     fontWeight: "500",
-    marginLeft:10
+    marginLeft: 10
   },
   rightbar: {
     flexDirection: "row",
     justifyContent: "space-around",
     width: '25%',
-    marginRight:10
+    marginRight: 10
   },
 });
